176af50dfcd6e51f63e0cf160f8218af2f7adbec
Patrick Pacher <patrick.pacher@gmail.com>
2020-12-07 09:01:08 +0100

    Add caddy-matomo plugin

--- caddy.go
+++ caddy.go
@@ -3,9 +3,10 @@ package main
 import (
 	"github.com/caddyserver/caddy/caddy/caddymain"
 
+	_ "github.com/abiosoft/caddy-git"
 	_ "github.com/dhaavi/caddy-permission"
-  _ "github.com/abiosoft/caddy-git"
 	_ "github.com/lucaslorentz/caddy-docker-proxy/plugin"
+	_ "github.com/tierklinik-dobersberg/caddy-matomo"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/caddyserver/caddy v1.0.5
 	github.com/dhaavi/caddy-permission v0.6.0
 	github.com/lucaslorentz/caddy-docker-proxy/plugin v0.0.0-20200307194201-c12eedfb5987
+	github.com/tierklinik-dobersberg/caddy-matomo v0.0.0-20200208201858-2577b629dd64
 )
